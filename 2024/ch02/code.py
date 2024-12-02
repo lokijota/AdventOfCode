eb71654dd84a562916093e1adfd6e89e8c3449d1
Jota <joao.pedro.martins@gmail.com>
2024-12-02 13:27:47 +0000

    Ch2 part 2 -- Got confused thinking of an algorithm and trying to go over the list only once, but just gave up. It's a small dataset anyway, runs well under subsecond. Ugly code, tho

--- 2024/ch02/code.py
+++ 2024/ch02/code.py
@@ -59,5 +59,53 @@ print("--- %s seconds ---" % (time.time() - start_time))
 start_time = time.time()
 result = 0
 
+def count_infractions(deltas):
+    inf = 0
+    removal_candidates = set()
+
+    for index, el in enumerate(deltas):
+        if el<=0 or el>3:
+            inf += 1
+            removal_candidates.add(index)
+            removal_candidates.add(index+1)
+
+    return inf, removal_candidates
+
+    # return sum(1 for el in data if el<=0 or el>3)
+
+count = 0
+for line in lines:
+    row = [int(el) for el in line.split(" ")]
+    row_pair_deltas = [int(y)-int(x) for (x, y) in pairwise(row)]
+
+    # to make things simpler I can flip all the numbers if negative
+    negative_count = sum(1 for el in row_pair_deltas if el<0)
+    if negative_count>1:
+        row = [-el for el in row]
+        row_pair_deltas = [-el for el in row_pair_deltas]
+
+    # ok so now all the sequences are increasing
+    res, removal_candidates = count_infractions(row_pair_deltas)
+    if res == 0:
+        count += 1
+        continue
+        
+    for rc in removal_candidates:
+        removed_el = row[rc]
+        row.pop(rc)
+
+        row_pair_deltas = [int(y)-int(x) for (x, y) in pairwise(row)]
+        res, _ = count_infractions(row_pair_deltas)
+
+        # if we have one removal that removes the inconsistencies, we're fine
+        if res == 0:
+            count += 1
+            break
+        else:
+            row.insert(rc, removed_el)
+            # and continue checking
+
+result = count
+
 print("Result part 2: ", result) #
 print("--- %s seconds ---" % (time.time() - start_time))
