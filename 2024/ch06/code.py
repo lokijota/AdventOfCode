3b020ffe029b7ca3a266174d3c928e6cb84472b4
Jota <joao.pedro.martins@gmail.com>
2024-12-06 20:17:56 +0100

    Scaffolding ch06

---
+++ 2024/ch06/code.py
@@ -0,0 +1,43 @@
+import time
+from collections import deque, Counter
+import sys
+import numpy as np
+import re
+import copy
+from tqdm import tqdm
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+from itertools import pairwise
+import math
+
+## read data
+
+## global variables
+
+with open('ch06/input.txt') as f:
+    lines = f.read().splitlines()
+
+## parse data
+
+## functions
+
+
+## part 1
+
+start_time = time.time()
+result = 0
+
+
+print("Result part 1: ", result) #
+print("--- %s seconds ---" % (time.time() - start_time))
+
+## part 2
+
+start_time = time.time()
+result = 0
+
+
+
+print("Result part 2: ", result) #
+print("--- %s seconds ---" % (time.time() - start_time))
