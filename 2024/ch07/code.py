32cdaec6775808552e76e5f6bda485a23c945ff2
Jota <joao.pedro.martins@gmail.com>
2024-12-07 18:48:02 +0100

    Ch07, runs in 0.5secs. Could be optimized to avoid repeating operations

--- 2024/ch07/code.py
+++ 2024/ch07/code.py
@@ -20,12 +20,42 @@ with open('ch07/input.txt') as f:
 
 ## parse data
 
+data = []
+
+for line in lines:
+    parts = line.split(":")
+    data.append((int(parts[0]), [int(val.strip()) for val in parts[1].lstrip().split(" ")]))
+
 ## functions
 
 ## part 1
 
 start_time = time.time()
 result = 0
+
+for row in data:
+    for j in range(2**len(row[1])):
+        
+        register = row[1][0]
+
+        for val in row[1][1:]:
+            op = j & 1
+            j = j >> 1
+
+            if op == 0:
+                register += val
+            else:
+                register *= val
+            
+            if register > row[0]:
+                break
+        
+            # print(op, register)
+        
+        if register == row[0]:
+            # print("found")
+            result += row[0]
+            break
 
 print("Result part 1: ", result) #
 print("--- %s seconds ---" % (time.time() - start_time))
