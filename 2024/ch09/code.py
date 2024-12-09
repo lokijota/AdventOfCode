5fb05b86dfa4c59e25b67dcbe2cdce6df1f40a7b
Jota <joao.pedro.martins@gmail.com>
2024-12-09 22:52:30 +0100

    Scaffolding ch09

---
+++ 2024/ch09/code.py
@@ -0,0 +1,44 @@
+import time
+from collections import deque, Counter
+import sys
+import numpy as np
+import re
+import copy
+from tqdm import tqdm
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+import itertools
+import math
+import operator
+
+## read data
+
+## global variables
+
+with open('ch09/sample.txt') as f:
+    lines = f.read().splitlines()
+
+## parse data
+
+## functions
+
+## part 1
+
+start_time = time.time()
+result = 0
+
+# create a dictionary where t
+
+print("Result part 1: ", result) #
+print("--- %s seconds ---" % (time.time() - start_time))
+
+## part 2
+
+start_time = time.time()
+result = 0
+
+
+
+print("Result part 2: ", result) #
+print("--- %s seconds ---" % (time.time() - start_time))
