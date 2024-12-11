66e2259b9c6efd7353007de42da4d51dcfe655c0
Jota <joao.pedro.martins@gmail.com>
2024-12-11 22:27:40 +0100

    Ch11 parts 1 and 2 simple and fast using just hashtable to register frequencies

--- 2024/ch11/code.py
+++ 2024/ch11/code.py
@@ -15,10 +15,14 @@ from tqdm import tqdm
 
 ## global variables
 
-with open('ch11/sample.txt') as f:
-    lines = f.read().splitlines()
+with open('ch11/input.txt') as f:
+    entries = f.read().split(" ")
 
 ## parse data
+
+data = dict()
+for entry in entries:
+    data[int(entry)] = 1
 
 ## functions
 
@@ -27,15 +31,41 @@ with open('ch11/sample.txt') as f:
 start_time = time.time()
 result = 0
 
-        
+# 25 for part 1, 75 for part 2 -- code is the same
+for j in range(75):
+    new_data = dict()
+
+    for key,count in data.items():
+        # rule 1
+        if key == 0:
+            new_data[1] = new_data.get(1, 0) + count
+        # rule 2
+        elif len(str(key)) % 2 == 0:
+
+            key_s = str(key)
+
+            left  = int(key_s[0:len(key_s)//2])
+            right = int(key_s[len(key_s)//2:])
+
+            new_data[left] = new_data.get(left, 0) + count
+            new_data[right] = new_data.get(right, 0) + count
+        # rule 3
+        else:
+            new_data[key*2024] = new_data.get(key*2024, 0) + count
+
+    data = new_data
+
+    # print(j+1, sum(data.values()))
+
+result = sum(data.values())
 print("Result part 1: ", result) #
 print("--- %s seconds ---" % (time.time() - start_time))
 
 ## part 2
 
-start_time = time.time()
-result = 0
+# start_time = time.time()
+# result = 0
 
 
-print("Result part 2: ", int(result)) #
-print("--- %s seconds ---" % (time.time() - start_time))
+# print("Result part 2: ", int(result)) #
+# print("--- %s seconds ---" % (time.time() - start_time))
