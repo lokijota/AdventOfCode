5c92f4ba7e0258143b8cfd1a797f7b49f16f25dc
Jota <joao.pedro.martins@gmail.com>
2024-12-20 00:05:45 +0100

    Scaffolding ch17

---
+++ 2024/ch17/code.py
@@ -0,0 +1,41 @@
+import copy
+import itertools
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import numpy as np
+import random
+import re
+import sys
+import time
+# from collections import deque, Counter
+# from tqdm import tqdm
+# from operator import add, sub
+# import networkx as nx
+
+## read data
+
+## global variables
+
+with open('ch17/sample.txt') as f:
+    lines = f.read().splitlines()
+
+## parse data
+
+## functions
+
+## part 1
+
+start_time = time.time()
+result = 0
+
+print("Result part 1: ", result) 
+print("--- %s seconds ---" % (time.time() - start_time))
+
+## part 2
+
+start_time = time.time()
+result = 0
+
+print("Result part 2: ", int(result)) 
+print("--- %s seconds ---" % (time.time() - start_time))
