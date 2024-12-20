10aa93bc432474001cde965ededfacc0b7be9441
Jota <joao.pedro.martins@gmail.com>
2024-12-20 01:00:33 +0100

    Ch17 simple. Reminds me of uni: compilers with HL.

--- 2024/ch17/code.py
+++ 2024/ch17/code.py
@@ -17,19 +17,91 @@ import time
 
 ## global variables
 
-with open('ch17/sample.txt') as f:
+with open('ch17/input.txt') as f:
     lines = f.read().splitlines()
 
 ## parse data
 
-## functions
+register_a = int(lines[0].split(": ")[1])
+register_b = int(lines[1].split(": ")[1])
+register_c = int(lines[2].split(": ")[1])
+bytecode = [int(instruction) for instruction in lines[4].split(": ")[1].split(",")]
+
+## functions and classes
+    
+class ChronospacialComputer:
+    """ My first class im this year's AoC. It's a been a while, my old friend. """
+    def __init__(self, register_a, register_b, register_c):
+        self.a = register_a
+        self.b = register_b
+        self.c = register_c
+        self.ip = 0
+        self.last_output = []
+        self.last_program = []
+    
+    def __str__(self):
+        separator = ', ' 
+        return separator.join(map(str, self.last_output)) 
+
+    def combo_operand(self, operand):
+        if operand <= 3:
+            return operand
+        elif operand == 4:
+            return self.a
+        elif operand == 5:
+            return self.b
+        elif operand == 6:
+            return self.c
+
+    def run(self, program):
+        self.last_program = program
+
+        while self.ip < len(program):
+
+            match program[self.ip]:
+                case 0:
+                    self.a = int(self.a / pow(2, self.combo_operand(program[self.ip+1])))
+                    self.ip += 2
+                case 1:
+                    self.b = self.b ^ program[self.ip+1]
+                    self.ip += 2
+                case 2:
+                    self.b = self.combo_operand(program[self.ip+1]) % 8
+                    self.ip += 2
+                case 3:
+                    if self.a != 0:
+                        self.ip = program[self.ip+1]
+                    else:
+                        self.ip += 2
+                case 4:
+                    self.b = self.b ^ self.c 
+                    self.ip += 2
+                case 5:
+                    self.last_output.append(self.combo_operand(program[self.ip+1])%8)
+                    self.ip += 2
+                case 6:
+                    self.b = int(self.a / pow(2, self.combo_operand(program[self.ip+1])))
+                    self.ip += 2
+                case 7:
+                    self.c = int(self.a / pow(2, self.combo_operand(program[self.ip+1])))
+                    self.ip += 2
+
+        return self.last_output
+
+
 
 ## part 1
 
 start_time = time.time()
 result = 0
 
-print("Result part 1: ", result) 
+prog = ChronospacialComputer(register_a, register_b, register_c)
+output = prog.run(bytecode)
+print("Output: ", output)
+result = len(output)
+
+
+print("Result part 1: ", result)  # 7,3,0,5,7,1,4,0,5
 print("--- %s seconds ---" % (time.time() - start_time))
 
 ## part 2
