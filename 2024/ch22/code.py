dc1231651aa8a8b971b7801431950c1e193dddd0
Jota <joao.pedro.martins@gmail.com>
2024-12-30 22:10:02 +0100

    ch22 - simple, as expected

--- 2024/ch22/code.py
+++ 2024/ch22/code.py
@@ -16,19 +16,35 @@ from tqdm import tqdm
 
 ## global variables
 
-with open('ch22/sample.txt') as f:
+with open('ch22/input.txt') as f:
     lines = f.read().splitlines()
 
 ## parse data
 
 ## functions and classes
 
+def random_number_generator(seed):
+    nb = ((seed*64)^seed) % 16777216
+    nb = int(nb/32)^nb
+    nb = ((nb*2048)^nb)%16777216
+    return nb
+
 ## part 1
 
 start_time = time.time()
 result = 0
 
-print("Result part 1: ", result)  
+# print(random_number_generator(123))
+
+for random_nb in lines:
+    random_nb = int(random_nb)
+    for _ in range(0, 2000):
+        random_nb = random_number_generator(random_nb)
+
+    print(random_nb) 
+    result += random_nb
+
+print("Result part 1: ", result) # 13584398738
 print("--- %s seconds ---" % (time.time() - start_time))
 
 ## part 2
