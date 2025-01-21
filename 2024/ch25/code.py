26b535536ce7c25e8f671e5248e0aaf0864c677b
Jota <joao.pedro.martins@gmail.com>
2025-01-21 23:16:30 +0100

    ch25 part 1, strangely simple

--- 2024/ch25/code.py
+++ 2024/ch25/code.py
@@ -15,9 +15,34 @@ from tqdm import tqdm
 ## read data
 
 with open('ch25/input.txt') as f:
-    lines = f.read().split("\n\n")
+    records = f.read().split("\n\n")
 
 ## parse data
+
+lock_shapes = []
+lock_counts = []
+
+key_shapes = []
+key_counts = []
+
+for entry in records:
+    # read elements to a list, reshape to the shape of the lock, and remove first and last rows which are just markers
+    marker = entry[0]
+
+    record_shape = np.reshape([element for element in entry if element in ["#", "."]], (7,5))[1:-1]
+
+    counts = []
+    for column in record_shape.T:
+        counts.append(np.count_nonzero(column == "#"))
+    
+    if marker == "#": #lock
+        lock_shapes.append(record_shape)
+        lock_counts.append(counts)
+
+    else: #key
+        key_shapes.append(record_shape)
+        key_counts.append(counts)
+
 
 ## functions and classes
 
@@ -26,8 +51,13 @@ with open('ch25/input.txt') as f:
 start_time = time.time()
 result = 0
 
+for the_key in key_counts:
+    for the_lock in lock_counts:
+        print("Comparing", the_key, the_lock)
+        if all(kh+lh <= 5 for kh,lh in zip(the_key,the_lock)):
+            result += 1
 
-print("Result part 1: ", result) 
+print("Result part 1: ", result) #3127
 print("--- %s seconds ---" % (time.time() - start_time))
 
 ## part 2
