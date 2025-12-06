d0a0f45819eb028a136f6bfbf7342c9fb76f18e4
Jota <joao.pedro.martins@gmail.com>
2025-12-06 19:35:49 +0100

    Day 1 done. Simple. Modulos and stuff. There's prob a more compact way, but.

--- 2025/ch01/code.py
+++ 2025/ch01/code.py
@@ -32,10 +32,22 @@ print(data)
 start_time = time.time()
 result = 0
 
+current_pos = 50
+cross_zero_count = 0
 
+for rotation in data:
+    if rotation[0] == 'L':
+        current_pos -= int(rotation[1:]) 
+    else:
+        current_pos += int(rotation[1:])
 
+    current_pos = current_pos % 100
+    if current_pos == 0:
+        cross_zero_count += 1
 
-print("Result part 1: ", result) #
+result = cross_zero_count
+
+print("Result part 1: ", result) # 962
 print("--- %s seconds ---" % (time.time() - start_time))
 
 ## part 2
@@ -43,9 +55,28 @@ print("--- %s seconds ---" % (time.time() - start_time))
 start_time = time.time()
 result = 0
 
+current_pos = 50
+cross_zero_count = 0
 
+for rotation in data:
+    clicks = int(rotation[1:])
+    cross_zero_count += clicks//100 # floor division to count full circles
+    clicks = clicks % 100 # remaining clicks after full circles
 
+    prev_pos = current_pos
 
+    if rotation[0] == 'L':
+        current_pos -= clicks 
+    else:
+        current_pos += clicks
+    
+    if prev_pos != 0:
+        if current_pos <= 0 or current_pos >= 100:
+            cross_zero_count += 1
 
-print("Result part 2: ", result) #
+    current_pos = current_pos % 100
+
+result = cross_zero_count
+
+print("Result part 2: ", result) # 5782
 print("--- %s seconds ---" % (time.time() - start_time))
