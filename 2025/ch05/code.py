1a96e29f730c7883df97852be4546258bd570478
Jota <joao.pedro.martins@gmail.com>
2025-12-07 18:19:58 +0100

    Day 5 scaffolding

---
+++ 2025/ch05/code.py
@@ -0,0 +1,46 @@
+import sys
+import time
+# assuming jotalibrary.py is in the same directory / for the code to work we run the python from the top level directory
+sys.path.append(".") 
+from jotalibrary import *
+
+from itertools import groupby
+import numpy as np
+# from collections import deque, Counter
+# import sys
+# import re
+# import copy
+# from tqdm import tqdm
+# import random
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+## read data
+
+## global variables
+
+with open('ch05/sample.txt') as f:
+    lines = f.read().splitlines()
+
+data = lines
+
+# print(data)
+## functions
+
+## part 1
+
+start_time = time.time()
+result = 0
+
+
+print("Result part 1: ", result) # 
+print("--- %s seconds ---" % (time.time() - start_time))
+
+## part 2
+
+start_time = time.time()
+result = 0
+
+
+print("Result part 2: ", result) 
+print("--- %s seconds ---" % (time.time() - start_time))
