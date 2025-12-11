f7bab6f3036ad877a39ba06cfb04c22d87e621e2
Jota <joao.pedro.martins@gmail.com>
2025-12-11 01:22:11 +0100

    Day 5, simple, fast

--- 2025/README.md
+++ 2025/README.md
@@ -20,5 +20,7 @@ Setup steps:
 - **Day 02**: Simple, they key thing here for part 2 was to use an auxiliary structure to note where to search for repeated sequences. Could be faster, but even so it's under 2 secs.
 - **Day 03**: Did a first implementation that did loops, but this didn't work for the second part. Final implementation is fast for both cases (same code, one variable configuration is different), and essentially uses the max of a progressing slice to find each of the highest digits. A few ms run-time for both cases.
 - **Day 04**: Happy with this one. Each part runs under 0.02s. Use an auxiliary bidim array with +2 dimension to avoid borders, and that array has the number of surrounding rolls. For part 2 I just have to consecutively substract to this, using a large number to mark already removed rolls. Quick and elegant (using more memory, though)
+- **Day 05**: Again the topic of ranges. Simple and fast. Could probably also have used https://pypi.org/project/portion/ again, but not worth it.
+
 
 Also: coding in vscode using **vim**, for old-times sake.
--- 2025/ch05/code.py
+++ 2025/ch05/code.py
@@ -6,7 +6,8 @@ from jotalibrary import *
 
 from itertools import groupby
 import numpy as np
-# from collections import deque, Counter
+from collections import deque 
+# from collections import Counter
 # import sys
 # import re
 # import copy
@@ -19,12 +20,26 @@ import numpy as np
 
 ## global variables
 
-with open('ch05/sample.txt') as f:
+with open('ch05/input.txt') as f:
     lines = f.read().splitlines()
 
-data = lines
+id_ranges = []
+ingredient_ids = []
 
-# print(data)
+processing_ranges = True
+for line in lines:
+    if line == "":
+        processing_ranges = False
+        continue
+
+    if processing_ranges:
+        id_ranges.append([int(x) for x in line.split("-")])
+    else: 
+        ingredient_ids.append(int(line))
+
+print(id_ranges)
+print(ingredient_ids)
+
 ## functions
 
 ## part 1
@@ -32,8 +47,13 @@ data = lines
 start_time = time.time()
 result = 0
 
+for ingredient_id in ingredient_ids:
+    for id_range in id_ranges:
+        if ingredient_id >= id_range[0] and ingredient_id <= id_range[1]:
+            result += 1
+            break
 
-print("Result part 1: ", result) # 
+print("Result part 1: ", result) # 664
 print("--- %s seconds ---" % (time.time() - start_time))
 
 ## part 2
@@ -41,6 +61,30 @@ print("--- %s seconds ---" % (time.time() - start_time))
 start_time = time.time()
 result = 0
 
+id_ranges_merged = []
 
-print("Result part 2: ", result) 
+print(id_ranges)
+id_ranges = sorted(id_ranges, key=lambda x: x[0])
+print(id_ranges)
+
+id_queue = deque(id_ranges)
+
+while id_queue:
+    current_range = id_queue.popleft()
+
+    while id_queue:
+        next_range = id_queue[0]
+        if current_range[1] < next_range[0]:
+            id_ranges_merged.append(current_range)
+            break
+        else:
+            current_range[1] = max(current_range[1], next_range[1])
+            id_queue.popleft()
+
+    if not id_queue:
+        id_ranges_merged.append(current_range)
+    
+result = sum([r[1] - r[0] + 1 for r in id_ranges_merged])
+
+print("Result part 2: ", result)  # 350780324308385
 print("--- %s seconds ---" % (time.time() - start_time))
