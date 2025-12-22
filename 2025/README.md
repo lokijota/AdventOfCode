277087324b9b1e8e894747bdcc36648eb0fb685e
Jota <joao.pedro.martins@gmail.com>
2025-12-22 23:19:24 +0100

    Day 11 done with Scipy/linprog and a variant of Simplex. Fast execution.

--- 2025/README.md
+++ 2025/README.md
@@ -25,6 +25,7 @@ Setup steps:
 - **Day 07**: The Tachyons one with the beam splitter. Second part felt more complicated than it is really, and also runs in ms times.
 - **Day 08**: This is essentially KNN (clustering). Spent some time with silly bugs, but the approach is simple. 22 secs part 1, 45 secs part 2.
 - **Day 09**: Part 1 is simple using a simple brute force approach, part 2 took me a LOT of work and it still takes 50mins to get to the result. Essentially I create a matrix based on all turn points (red tiles), then compress the map based on this information, then iterate on rectangles like in part 1 but on a much smaller (compressed) map. Use matplotlib Path to determine if something is inside the shape (is this cheating?)
+- **Day 10**: Part 1 with trees and my trusted `deque`, part 2 with `scipy.optimize.linprog`, using a variant of Simplex. I quickly realized the approach from part 1 didn't work in part 2, so went googling for mathematical approaches and Simplex led me to the right place. A couple of Stackoverflow answers plus the docs helped me figure it out. Part 1 takes under 2 seconds, part 2 takes about 0.1 sec.
 
 
 Also: coding in vscode using **vim**, for old-times sake.
--- 2025/ch10/code.py
+++ 2025/ch10/code.py
@@ -4,10 +4,11 @@ import time
 sys.path.append(".") 
 from jotalibrary import *
 
-from itertools import groupby
 import numpy as np
 from collections import deque 
-import math
+from scipy.optimize import linprog
+# from itertools import groupby
+# import math
 # from collections import Counter
 # import sys
 # import re
@@ -33,10 +34,6 @@ class Machine:
 
         self.indices_of_on_lights = [idx for idx, character in enumerate(self.light_diagram) if character == "#"]
 
-        # def set_bit(value, bit):
-        #     return value | (1<<bit)
-        # def clear_bit(value, bit):
-        #     return value & ~(1<<bit)
         self.target_configuration = 0
         for idx in self.indices_of_on_lights:
             self.target_configuration |= (1<<idx)
@@ -50,6 +47,16 @@ class Machine:
         # print('{:b}'.format(light_config))
         return light_config
 
+    def press_joltages(self, button_presses):
+        lights = np.zeros(len(self.joltage_requirements), dtype=int)
+
+        for idxbp, bp in enumerate(button_presses):
+            for bw in self.button_wirings[idxbp]:
+                lights[bw] += bp 
+
+        # for idx in button_presses:
+        #     current_joltages[idx] += 1
+        return lights
 
 manual = []
 # [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
@@ -68,10 +75,10 @@ for line in lines:
 start_time = time.time()
 result = 0
 
-
 for machine in manual:
     bfs = deque()
     bfs.append([0, 0])
+    # break # go to part 2
 
     while len(bfs)>0:
         head = bfs.popleft()
@@ -87,7 +94,7 @@ for machine in manual:
             else:
                 bfs.append([val, head[1]+1])
 
-print("Result part 1: ", result) # 
+print("Result part 1: ", result) # 530
 print("--- %s seconds ---" % (time.time() - start_time))
 
 ## part 2
@@ -95,7 +102,58 @@ print("--- %s seconds ---" % (time.time() - start_time))
 start_time = time.time()
 result = 0
 
+# Using Simplex algorithm as per the code example here
+# https://stackoverflow.com/questions/45873783/python-linprog-minimization-simplex-method
+# And this example was useful to clarify how to use with equalities
+# https://stackoverflow.com/questions/46702953/linear-programming-using-scipy-optimize-linprog-returns-suboptimal-solution?rq=3
 
+for machine in manual:
+    
+    coefficient_matrix = np.zeros(shape=(len(machine.joltage_requirements), len(machine.button_wirings)) , dtype=int)
 
-print("Result part 2: ", result)  # 
+    for idxbw, bw in enumerate(machine.button_wirings):
+        for button in bw:
+            coefficient_matrix[button, idxbw] = 1
+
+    # identity_matrix = np.eye(len(machine.button_wirings), dtype=int)
+    # A_ub = np.concatenate((coefficient_matrix, identity_matrix))*-1
+    # As the positiveness of y1 and y2 can be guaranteed under bounds=(0, None), we can remove the extra values
+    A_ub = coefficient_matrix*-1
+    # print(A_ub)
+
+    # b_ub = np.array(machine.joltage_requirements + [0]*len(machine.button_wirings))*-1
+    # As the positiveness of y1 and y2 can be guaranteed under bounds=(0, None), we can remove the extra values
+    b_ub = np.array(machine.joltage_requirements)*-1
+    # print(b_ub)
+
+    c = np.ones(len(machine.button_wirings), dtype=int)
+    # print(c)
+
+    # optimize.linprog *always* minimizes your target function
+    # important note: the line below uses equalities, in the equations, while the other one assumes >= and gives the wrong result
+    res = linprog(c, A_eq = A_ub, b_eq = b_ub, integrality=3, bounds=(0, max(machine.joltage_requirements)))
+    # res = linprog(c, A_ub, b_ub, integrality=3, bounds=(0, max(machine.joltage_requirements)))
+
+    # simulate the button presses in each result, to compare
+    joltages_after_presses = machine.press_joltages(res.x.astype(int))
+    if (machine.joltage_requirements!=joltages_after_presses).any():
+
+        # weirdly, simulating the button presses, gives the wrong result for 4 of the machines, eg:
+
+        # Joltages:  [56, 74, 68, 51, 33, 39, 58, 48, 52, 69]
+        #            [54 72 66 49 32 37 56 47 50 67]
+        # Optimal value: 95 <-- this is number I'm adding to the result
+        # Presses (X): [ 0  0  4 11 13 19  2 13  4 14 11  1  0] , Sum presses= 92 <- see how it gives 3 under the Optimal value returned!!
+        # Success: True , Status: 0 , Message: Optimization terminated successfully. (HiGHS Status 7: Optimal)
+        # N Iterations: 12
+
+        print("--------\nJoltages: ", machine.joltage_requirements)
+        print("          ", joltages_after_presses)
+        print('Optimal value:', int(res.fun), '\nPresses (X):', res.x.astype(int), ", Sum presses=", sum(res.x.astype(int)))
+        print("Success:", res.success, ", Status:", res.status, ", Message:", res.message)
+        print("N Iterations:", res.nit)
+
+    result += int(res.fun)
+
+print("Result part 2: ", result)  # 20172
 print("--- %s seconds ---" % (time.time() - start_time))
