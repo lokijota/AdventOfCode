a425815af5980a186f5c83471f5a4065daebe016
Jota <joao.pedro.martins@gmail.com>
2025-12-30 23:03:36 +0100

    2015 day 1, parts 1 and 2. Ultra simple

---
+++ 2015/README.md
@@ -0,0 +1,23 @@
+# Advent Of Code 2015
+
+Coding in Python 3.14.0. Trying uv (https://docs.astral.sh/uv/) as a package manager. 
+
+# uv
+
+Setup steps:
+
+- Installed as per guidance on `uv` docs.
+- Created a folder for AOC 2015 and cd'd into it
+- `uv init` to create the project in the current
+- Updated the contents of `pyproject.toml`
+- `uv add numpy` to add python packages
+- `source .venv/bin/activate` to activate the environment (needed before running the code)
+- If there is an error with Vs Code not using the right Python environment, do: View > Command Pallete > Python: Select Interpreter.
+
+# Notes on the execises
+
+- **Day 01**: 
+
+Another year completed! /happy.
+
+Also: coding in vscode using **vim**, for old-times sake.
---
+++ 2015/ch01/code.py
@@ -0,0 +1,57 @@
+import sys
+import time
+# assuming jotalibrary.py is in the same directory / for the code to work we run the python from the top level directory
+sys.path.append(".") 
+from jotalibrary import *
+
+# from itertools import groupby
+# import numpy as np
+# from collections import deque 
+# import math
+# from collections import Counter
+# import sys
+# import re
+# import copy
+# from tqdm import tqdm
+# import random
+# import matplotlib.pyplot as plt
+# from matplotlib import path
+
+## read data
+
+## global variables
+
+with open('ch01/input.txt') as f:
+    lines = f.read().splitlines()
+
+## functions
+
+
+## part 1
+
+start_time = time.time()
+result = 0
+
+result = lines[0].count('(') - lines[0].count(')')
+
+print("Result part 1: ", result) # 138
+print("--- %s seconds ---" % (time.time() - start_time))
+
+## part 2
+
+start_time = time.time()
+result = 0
+
+for idx, char in enumerate(lines[0]):
+    if char == '(':
+        result += 1
+    elif char == ')':
+        result -= 1
+
+    if result == -1:
+        result = idx + 1
+        break
+
+
+print("Result part 2: ", result)  # 
+print("--- %s seconds ---" % (time.time() - start_time))
