2f46cd8c9557df53568a4585168f62f0a93f867c
Jota <joao.pedro.martins@gmail.com>
2025-12-30 20:45:04 +0100

    High level heuristics based on area

--- 2025/ch12/code.py
+++ 2025/ch12/code.py
@@ -21,8 +21,36 @@ import math
 
 ## global variables
 
-with open('ch12/input.txt') as f:
+with open('ch12/sample.txt') as f:
     lines = f.read().splitlines()
+
+reading_shapes = True
+
+shapes = []
+counts = []
+boxes = []
+
+for line in lines:
+
+    if "x" in line:
+        reading_shapes = False
+
+    if reading_shapes:
+        if ":" in line:
+            shapes.append([])
+            counts.append(0)
+        elif len(line) == 0:
+            continue
+        else:
+            shapes[-1].append(line)
+            counts[-1] += line.count("#")
+    else:
+        parts = line.split(":")
+        sizes = parts[0].split("x")
+        boxes.append([(int(sizes[0]), int(sizes[1]))])
+
+        part_counts = parts[1].split()
+        [boxes[-1].append(int(item)) for item in part_counts]
 
 ## functions
 
@@ -31,6 +59,28 @@ with open('ch12/input.txt') as f:
 
 start_time = time.time()
 result = 0
+
+# print some statistics
+fit_for_sure = 0
+
+for box in boxes:
+    rect_area = box[0][0] * box[0][1]
+    part_area = 0
+
+    three_multiple_box_area = box[0][0]//3*3 * box[0][1]//3*3
+    part_count_area = sum(box[1:])*9
+    
+    for idx, part_count in enumerate(box[1:]):
+        part_area += part_count * counts[idx]
+    
+    print(f"Rectangle area: {rect_area}, Parts area={part_area}, diff = {rect_area-part_area}") 
+    print(f"    3x3 part area= {part_count_area}, enclosing area 3x3= {three_multiple_box_area}, it fits for sure={part_count_area<= three_multiple_box_area}")
+    if part_count_area<=three_multiple_box_area:
+        fit_for_sure += 1
+
+
+print(f"Total fits: {len(boxes)}, total fit for sure: {fit_for_sure}")
+
 
 
 print("Result part 1: ", result) # 
